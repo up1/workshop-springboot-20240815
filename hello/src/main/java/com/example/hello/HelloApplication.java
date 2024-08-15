024786233b2cf4f59dbe0f530f2d4c6f6bb26e07
up1 <somkiat.p@gmail.com>
2024-08-15 14:56:59 +0700

    Count beans from spring context

--- hello/src/main/java/com/example/hello/HelloApplication.java
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -2,12 +2,15 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(HelloApplication.class, args);
+		System.out.println("# of beans=" + context.getBeanDefinitionCount());
 	}
 
 }
