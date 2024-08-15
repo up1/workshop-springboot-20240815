185dbda5c6bf55bcb4c1d652fa12e33f04242c5e
up1 <somkiat.p@gmail.com>
2024-08-15 16:40:44 +0700

    Try to mock status of DB

--- hello/src/test/java/com/example/hello/employee/EmployeeRepositoryMockTest.java
+++ hello/src/test/java/com/example/hello/employee/EmployeeRepositoryMockTest.java
@@ -4,21 +4,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 class EmployeeRepositoryMockTest {
 
-    @Autowired
+    @MockBean
     private EmployeeRepository repository;
-    
+
     @Test
     @DisplayName("Database มีปัญหา โดยโยน RuntimeException ออกมา")
     public void case06() {
-        repository.findById(3);
+        // Arrange
+        when(repository.findById(3)).thenThrow(new RuntimeException());
+        // Act and Assert
+        assertThrows(RuntimeException.class, () -> {
+            repository.findById(3);
+        });
     }
 
 }
