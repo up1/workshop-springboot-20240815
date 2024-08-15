4692348479cda60bf789f77873d428786cd19f2f
up1 <somkiat.p@gmail.com>
2024-08-15 14:31:55 +0700

    Add test employee not found

---
+++ hello/src/test/java/com/example/hello/employee/EmployeeFailureControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.hello.employee;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ProblemDetail;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class EmployeeFailureControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    @DisplayName("ไม่พบข้อมูลของพนักงาน id=5")
+    void case02() {
+        ProblemDetail result
+                = restTemplate.getForObject("/employee/5", ProblemDetail.class);
+        assertEquals(404, result.getStatus());
+        assertEquals("Employee not found", result.getDetail());
+        assertEquals("/employee/5", result.getInstance().toString());
+    }
+}
