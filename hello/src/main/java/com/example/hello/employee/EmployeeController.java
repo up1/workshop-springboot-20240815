b336d97e7da36498b6f58edcd3c43498dd21af05
up1 <somkiat.p@gmail.com>
2024-08-16 10:01:55 +0700

    Demo with SBOM and structure log with logback

--- hello/src/main/java/com/example/hello/employee/EmployeeController.java
+++ hello/src/main/java/com/example/hello/employee/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.hello.employee;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.hello.aop.LogExecutionTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 
+    private final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
     private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
+    @LogExecutionTime
     @GetMapping("/employee/{id}")
     public EmployeeResponse getById(@PathVariable int id) {
+        logger.info("Handling getById request");
         EmployeeResponse employeeResponse = employeeService.getById(id);
         return employeeResponse;
     }
-
 
 }
