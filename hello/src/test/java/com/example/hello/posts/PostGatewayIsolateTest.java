006a22feab9f9e16ca9669d8c8872659a140f70f
up1 <somkiat.p@gmail.com>
2024-08-16 16:33:53 +0700

    Add spring cloud stub runner , wiremock

---
+++ hello/src/test/java/com/example/hello/posts/PostGatewayIsolateTest.java
@@ -0,0 +1,12 @@
+package com.example.hello.posts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostGatewayIsolateTest {
+
+    @Test
+    void getById() {
+    }
+}
